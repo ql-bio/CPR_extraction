8b8df2a3104963a5827829cb9fec0e6a61f2b63c
ql-bio <2535027681@qq.com>
2020-09-03 15:37:24 +0800

    code

---
+++ data/loader.py
@@ -0,0 +1,215 @@
+"""
+Data loader for TACRED json files.
+"""
+import sys
+sys.path.append("..")
+import json
+import random
+import torch
+import numpy as np
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+from utils import constant
+class DataLoader(object):
+    """
+    Load data from json files, preprocess and prepare batches.
+    """
+    def __init__(self, filename, batch_size, opt, vocab, evaluation=False):
+        self.batch_size = batch_size
+        self.opt = opt
+        self.vocab = vocab
+        self.eval = evaluation
+        self.label2id = constant.LABEL_TO_ID
+
+        with open(filename) as infile:
+            data = json.load(infile)
+        self.raw_data = data
+        tokenizer=BertTokenizer.from_pretrained(opt["bert_model_file"], do_lower_case=True)
+        data = self.preprocess(data, vocab, opt,tokenizer,evaluation)
+
+        #shuffle for training
+        if not evaluation:
+            indices = list(range(len(data)))
+            random.shuffle(indices)
+            data = [data[i] for i in indices]
+        self.id2label = dict([(v,k) for k,v in self.label2id.items()])
+        self.labels = [self.id2label[d[-1]] for d in data]
+        
+        self.num_examples = len(data)
+
+        # chunk into batches
+        data = [data[i:i+batch_size] for i in range(0, len(data), batch_size)]
+        self.data = data
+        print("{} batches created for {}".format(len(data), filename))
+
+    def preprocess(self, data, vocab, opt,tokenizer,evaluation):
+        """ Preprocess the data and convert to ids. """
+        processed = []
+        if not evaluation:
+            data = sorted(data, key=lambda f: len(f["token"]))
+        for d in data:
+            tokens = list(d['token'])
+            if opt['lower']:
+                tokens = [t.lower() for t in tokens]
+            # anonymize tokens
+            ss, se = d['subj_start'], d['subj_end']
+            os, oe = d['obj_start'], d['obj_end']
+            tokens[ss:se+1] = ['SUBJ-'+d['entity1_type']] * (se-ss+1)
+            tokens[os:oe+1] = ['OBJ-'+d['entity2_type']] * (oe-os+1)
+            tokens = map_to_ids(tokens, vocab.word2id)
+            
+            pos = map_to_ids(d['stanford_pos'], constant.POS_TO_ID)
+            ner = map_to_ids(d['stanford_ner'], constant.NER_TO_ID)
+            deprel = map_to_ids(d['stanford_deprel'], constant.DEPREL_TO_ID)
+            head = [int(x) for x in d['stanford_head']]
+            #判断head中是否有0，若没有0则出错
+            assert any([x == 0 for x in head])
+            l = len(tokens)
+            subj_positions = get_positions(d['subj_start'], d['subj_end'], l)
+            obj_positions = get_positions(d['obj_start'], d['obj_end'], l)
+            subj_type = [constant.SUBJ_NER_TO_ID[d['entity1_type']]]
+            obj_type = [constant.OBJ_NER_TO_ID[d['entity2_type']]]
+            relation = self.label2id[d['relation']]
+            input_ids,input_mask,segment_ids,label_id=getBertbacth(list(d['token']),opt,d['subj_start'],d['subj_end'],d['obj_start'],d['obj_end'],tokenizer,relation)
+            processed += [(tokens, pos, ner, deprel, head, subj_positions, obj_positions, subj_type,obj_type,relation,input_ids,input_mask,segment_ids,label_id)]
+        return processed
+
+    def gold(self):
+        """ Return gold labels as a list. """
+        return self.labels
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        """ Get a batch with index. """
+        if not isinstance(key, int):
+            raise TypeError
+        if key < 0 or key >= len(self.data):
+            raise IndexError
+        batch = self.data[key]
+        batch_size = len(batch)
+        batch = list(zip(*batch))
+        # assert len(batch) == 11
+
+        # sort all fields by lens for easy RNN operations
+        lens = [len(x) for x in batch[0]]
+        batch, orig_idx = sort_all(batch, lens)
+
+        # word dropout
+        if not self.eval:
+            words = [word_dropout(sent, self.opt['word_dropout']) for sent in batch[0]]
+        else:
+            words = batch[0]
+
+        # convert to tensors
+        words = get_long_tensor(words, batch_size)
+        masks = torch.eq(words, 0)
+        pos = get_long_tensor(batch[1], batch_size)
+        ner = get_long_tensor(batch[2], batch_size)
+        deprel = get_long_tensor(batch[3], batch_size)
+        head = get_long_tensor(batch[4], batch_size)
+        subj_positions = get_long_tensor(batch[5], batch_size)
+        obj_positions = get_long_tensor(batch[6], batch_size)
+        subj_type = get_long_tensor(batch[7], batch_size)
+        obj_type = get_long_tensor(batch[8], batch_size)
+        rels = torch.LongTensor(batch[9])
+        input_ids=torch.tensor(batch[10], dtype=torch.long)
+        input_mask=torch.tensor(batch[11], dtype=torch.long)
+        segment_ids=torch.tensor(batch[12], dtype=torch.long)
+        label_id=torch.tensor(batch[13], dtype=torch.long)
+        return (words, masks, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, rels, orig_idx,input_ids,input_mask,segment_ids,label_id)
+
+    def __iter__(self):
+        for i in range(self.__len__()):
+            yield self.__getitem__(i)
+
+
+def map_to_ids(tokens, vocab):
+    ids = [vocab[t] if t in vocab else constant.UNK_ID for t in tokens]
+    return ids
+
+
+def get_positions(start_idx, end_idx, length):
+    """ Get subj/obj position sequence. """
+    return list(range(-start_idx, 0)) + [0]*(end_idx - start_idx + 1) + \
+            list(range(1, length-end_idx))
+
+def getBertbacth(tokensss,opt,subj_start,subj_end,obj_start,obj_end,tokenizer,relation):
+    tokenss = [convert_token(token11) for token11 in tokensss]
+    max_seq_length=opt["max_seq_length"]
+    special_tokens={}
+    def get_special_token(w):
+        if w not in special_tokens:
+            special_tokens[w] = "[unused%d]" % (len(special_tokens) + 1)
+        return special_tokens[w]
+    SUBJECT_START = get_special_token("SUBJ_START")
+    SUBJECT_END = get_special_token("SUBJ_END")
+    OBJECT_START = get_special_token("OBJ_START")
+    OBJECT_END = get_special_token("OBJ_END")
+    tokens = ["[CLS]"]
+    for i, token in enumerate(tokenss):
+        if i == subj_start:
+            tokens.append(SUBJECT_START)
+        if i == obj_start:
+            tokens.append(OBJECT_START)
+        if (i >= subj_start) and (i <= subj_end):
+            pass
+        elif (i >=obj_start ) and (i <=obj_end ):
+            pass
+        else:
+            for sub_token in tokenizer.tokenize(token):
+                tokens.append(sub_token)
+    tokens.append("[SEP]")
+    if len(tokens) > max_seq_length:
+        tokens = tokens[:max_seq_length]
+    segment_ids = [0] * len(tokens)
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_mask = [1] * len(input_ids)
+    padding = [0] * (max_seq_length - len(input_ids))
+    input_ids += padding
+    input_mask += padding
+    segment_ids += padding
+    assert len(input_ids) == max_seq_length
+    assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
+    label_id=relation
+    return input_ids,input_mask,segment_ids,label_id
+    
+def convert_token(token):
+    """ Convert PTB tokens to normal tokens """
+    if (token.lower() == '-lrb-'):
+            return '('
+    elif (token.lower() == '-rrb-'):
+        return ')'
+    elif (token.lower() == '-lsb-'):
+        return '['
+    elif (token.lower() == '-rsb-'):
+        return ']'
+    elif (token.lower() == '-lcb-'):
+        return '{'
+    elif (token.lower() == '-rcb-'):
+        return '}'
+    return token
+    
+
+
+def get_long_tensor(tokens_list, batch_size):
+    """ Convert list of list of tokens to a padded LongTensor. """
+    token_len = max(len(x) for x in tokens_list)
+    tokens = torch.LongTensor(batch_size, token_len).fill_(constant.PAD_ID)
+    for i, s in enumerate(tokens_list):
+        tokens[i, :len(s)] = torch.LongTensor(s)
+    return tokens
+
+
+def sort_all(batch, lens):
+    """ Sort all fields by descending order of lens, and return the original indices. """
+    unsorted_all = [lens] + [range(len(lens))] + list(batch)
+    sorted_all = [list(t) for t in zip(*sorted(zip(*unsorted_all), reverse=True))]
+    return sorted_all[2:], sorted_all[1]
+
+
+def word_dropout(tokens, dropout):
+    """ Randomly dropout tokens (IDs) and replace them with <UNK> tokens. """
+    return [constant.UNK_ID if x != constant.UNK_ID and np.random.random() < dropout \
+            else x for x in tokens]
---
+++ utils/constant.py
@@ -0,0 +1,55 @@
+# # CPI_data
+# EMB_INIT_RANGE = 1.0
+
+# # vocab
+# PAD_TOKEN = '<PAD>'
+# PAD_ID = 0
+# UNK_TOKEN = '<UNK>'
+# UNK_ID = 1
+
+# VOCAB_PREFIX = [PAD_TOKEN, UNK_TOKEN]
+
+# # hard-coded mappings from fields to ids
+# SUBJ_NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'CHEMICAL': 2}
+
+# OBJ_NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'GENE-N': 2, 'GENE-Y': 3}
+
+# NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'O': 2, 'CHEMICAL': 3, 'GENE-N': 4, 'GENE-Y': 5}
+
+# POS_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'DT': 2, 'NN': 3, 'IN': 4, 'CC': 5, 'CD': 6, 'NNS': 7, ':': 8, 'VBG': 9, ',': 10, 'RB': 11, 'VBD': 12, 'VBN': 13, 'PRP$': 14, 'NNP': 15, 'JJ': 16, '.': 17, 'FW': 18, 'VBZ': 19, 'TO': 20, 'VB': 21, 'PRP': 22, 'VBP': 23, 'MD': 24, '``': 25, "''": 26, 'SYM': 27, 'EX': 28, 'WDT': 29, 'RP': 30, 'RBS': 31, 'PDT': 32, 'JJR': 33, 'LS': 34, 'POS': 35, 'JJS': 36, 'WRB': 37, 'RBR': 38, 'NNPS': 39, 'WP': 40, 'WP$': 41}
+
+# DEPREL_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'det': 2, 'nsubj': 3, 'prep': 4, 'amod': 5, 'pobj': 6, 'cc': 7, 'dep': 8, 'conj': 9, 'punct': 10, 'root': 11, 'dobj': 12, 'advmod': 13, 'mwe': 14, 'nsubjpass': 15, 'vmod': 16, 'auxpass': 17, 'poss': 18, 'nn': 19, 'num': 20, 'appos': 21, 'rcmod': 22, 'aux': 23, 'cop': 24, 'xcomp': 25, 'parataxis': 26, 'mark': 27, 'ccomp': 28, 'pcomp': 29, 'number': 30, 'expl': 31, 'acomp': 32, 'neg': 33, 'npadvmod': 34, 'prt': 35, 'iobj': 36, 'preconj': 37, 'advcl': 38, 'quantmod': 39, 'possessive': 40, 'csubj': 41, 'tmod': 42, 'predet': 43, 'csubjpass': 44, 'discourse': 45}
+# NEGATIVE_LABEL = 'false'
+
+# LABEL_TO_ID = {'false': 0, 'CPR:4': 1, 'CPR:3': 2, 'CPR:9': 3, 'CPR:6': 4, 'CPR:5': 5}
+
+# INFINITY_NUMBER = 1e12
+
+
+# tacred1
+EMB_INIT_RANGE = 1.0
+
+# vocab
+PAD_TOKEN = '<PAD>'
+PAD_ID = 0
+UNK_TOKEN = '<UNK>'
+UNK_ID = 1
+
+VOCAB_PREFIX = [PAD_TOKEN, UNK_TOKEN]
+
+# hard-coded mappings from fields to ids
+SUBJ_NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'drug': 2, 'group': 3,'brand':4,'drug_n':5}
+
+OBJ_NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'drug': 2, 'group': 3,'brand':4,'drug_n':5}
+
+NER_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'O': 2, 'drug': 3, 'group': 4, 'brand': 5, 'drug_n': 6}
+
+POS_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'NN': 2, 'NNS': 3, 'VBD': 4, 'TO': 5, 'VB': 6, 'JJ': 7, 'IN': 8, 'DT': 9, 'VBG': 10, ',': 11, 'VBP': 12, 'CC': 13, '.': 14, 'WRB': 15, 'VBN': 16, 'RB': 17, 'FW': 18, 'MD': 19, 'JJR': 20, 'VBZ': 21, ':': 22, 'NNP': 23, 'WDT': 24, 'CD': 25, 'SYM': 26, 'PRP': 27, 'PRP$': 28, 'RBS': 29, 'EX': 30, 'WP': 31, 'WP$': 32, 'NNPS': 33, 'RBR': 34, '``': 35, "''": 36, 'PDT': 37, 'LS': 38, 'POS': 39, 'JJS': 40, 'RP': 41}
+
+DEPREL_TO_ID = {PAD_TOKEN: 0, UNK_TOKEN: 1, 'amod': 2, 'nn': 3, 'nsubj': 4, 'root': 5, 'aux': 6, 'cop': 7, 'xcomp': 8, 'prep': 9, 'pobj': 10, 'mark': 11, 'det': 12, 'advcl': 13, 'punct': 14, 'conj': 15, 'cc': 16, 'advmod': 17, 'nsubjpass': 18, 'auxpass': 19, 'rcmod': 20, 'pcomp': 21, 'dobj': 22, 'dep': 23, 'mwe': 24, 'neg': 25, 'appos': 26, 'vmod': 27, 'ccomp': 28, 'parataxis': 29, 'num': 30, 'poss': 31, 'iobj': 32, 'npadvmod': 33, 'discourse': 34, 'quantmod': 35, 'expl': 36, 'acomp': 37, 'number': 38, 'preconj': 39, 'csubj': 40, 'tmod': 41, 'csubjpass': 42, 'predet': 43, 'possessive': 44, 'prt': 45}
+
+NEGATIVE_LABEL = 'false'
+
+LABEL_TO_ID={'false': 0, 'effect': 1, 'mechanism': 2, 'advise': 3, 'int': 4}
+
+INFINITY_NUMBER = 1e12
